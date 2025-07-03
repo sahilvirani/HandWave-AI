2b9d69cf75f90cc8dc19a401347e8d0fc343bdbe
Sahil Virani <sahilvirani2004@gmail.com>
2025-07-03 00:25:42 -0400

    chore: bootstrap HandWave AI React PWA scaffold

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import * as tf from '@tensorflow/tfjs';
+
+console.log('TensorFlow.js version:', tf.version.tfjs);
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
